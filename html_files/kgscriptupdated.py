1b22defe072540d05d90147473b7da9ab85eaf2e
Saifali Patel <61909924+saifali-patel@users.noreply.github.com>
2022-02-21 19:52:55 +0400

    Update kgscriptupdated.py

--- html_files/kgscriptupdated.py
+++ html_files/kgscriptupdated.py
@@ -219,7 +219,7 @@ def show(spec):
 
             
         else:
-          if test[datadict[0]["title"]]=nbr:
+          if test[datadict[0]["title"]]==nbr:
               if fixing_details[x]!=nbr:
                 test[x]=nbr
               else:
